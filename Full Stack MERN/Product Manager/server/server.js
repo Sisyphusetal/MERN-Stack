50df5c220bb6cd070f882fcfb901d1cb774fc413
Kurt James Clausen <kurtjclausen@gmail.com>
2023-07-20 16:52:27 -0700

    Product Manager part 1

--- Full Stack MERN/Product Manager/server/server.js
+++ Full Stack MERN/Product Manager/server/server.js
@@ -2,5 +2,14 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
+const cors = require('cors');
+require('./config/mongoose.config');
+
+app.use(cors())
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+require('./routes/product.routes')(app);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
